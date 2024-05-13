3764f8108a9dff10616cb1175cab24f38e9ee5c8
ryansurf <ryanfrederich@gmail.com>
2024-05-13 09:42:00 -0700

    sys arg input change

--- backend/helper.py
+++ backend/helper.py
@@ -3,7 +3,16 @@ import openmeteo_requests
 import requests_cache
 from retry_requests import retry    
 import requests
+from termcolor import colored
+import pandas as pd
 
+#Args are seperated by commas in input. Sereperat them and return list
+def seperate_args(args):
+    if len(args) > 1:
+        new_args = args[1].split(",")
+        return new_args
+    else:
+        return []
 # Takes a location(city or address) and returns the coordinates: [lat, long]
 #If no location is specified, default_location() finds the users coordinates
 def get_coordinates(args):
@@ -18,6 +27,18 @@ def get_coordinates(args):
             else:
                 return "No data"
     return default_location()
+
+#Checks to see if forecast in cli args. Defaults to 0
+def get_forecast_days(args):
+    for arg in args:
+        arg = str(arg)
+        if arg.startswith("forecast="):
+            forecast = int(arg.split('=')[1])
+            if forecast < 0 or forecast > 7:
+                print("Must choose a non-negative number >= 7 in forecast!")
+                break
+            return forecast
+    return 0
 
 #If no location specified in cli, find users location
 def default_location():
@@ -35,7 +56,7 @@ def default_location():
         return "No data"
 
 #Get UV at coordinates
-def get_uv(lat, long, decimal):
+def get_uv(lat, long, decimal, unit="imperial"):
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
@@ -47,6 +68,7 @@ def get_uv(lat, long, decimal):
     params = {
         "latitude": lat,
         "longitude": long,
+        "length_unit": unit,
         "current": "uv_index"
     }
     try:
@@ -68,7 +90,7 @@ def get_uv(lat, long, decimal):
     return current_uv_index
 
 #Get Ocean Data at coordinates
-def ocean_information(lat, long, decimal):
+def ocean_information(lat, long, decimal, unit="imperial"):
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
@@ -81,8 +103,8 @@ def ocean_information(lat, long, decimal):
         "latitude": lat,
         "longitude": long,
         "current": ["wave_height", "wave_direction", "wave_period"],
-        "length_unit": "imperial",
-        "timezone": "America/Los_Angeles",
+        "length_unit": unit,
+        "timezone": "auto",
         "forecast_days": 3
     }
     try:
@@ -92,10 +114,6 @@ def ocean_information(lat, long, decimal):
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    # print(f"Elevation {response.Elevation()} m asl")
-    # print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     # Current values. The order of variables needs to be the same as requested.
     current = response.Current()
@@ -123,15 +141,21 @@ def print_output(uv_index, ocean_data, show_uv, show_height, show_direction, sho
     if int(show_period) == 1:
         print("Wave Period: ", ocean_data[2])
 
-#Prints Wave
-def print_blue_wave(show_art):
-    if int(show_art) == 1:
-        BLUE = '\033[94m'  # ANSI escape code for blue color
-        RESET = '\033[0m'  # ANSI escape code to reset color
-        print(BLUE + "      .-``'." + RESET)
-        print(BLUE + "    .`   .`" + RESET)
-        print(BLUE + "_.-'     '._ " + RESET)
-        print()
+#Takes in list of forecast data and prints
+def print_forecast(forecast_list, uv_index, ocean_data, show_uv, show_height, show_direction, show_period, show_date):
+    transposed = list(zip(*forecast_list))
+    for day in transposed:
+        if show_date == 1:
+            print("Date: ", day[3])
+        # if int(show_uv) == 1:
+        #     print("UV index: ", uv_index)
+        if int(show_height) == 1:
+            print("Wave Height: ", day[0])
+        if int(show_direction) == 1:
+            print("Wave Direction: ", day[1])
+        if int(show_period) == 1:
+            print("Wave Period: ", day[2])
+        print("\n")
 
 # Function to extract decimal value from command-line arguments
 #Default is 1
@@ -145,8 +169,75 @@ def extract_decimal(args):
                 print("Invalid value for decimal. Please provide an integer.")
     return 1
 
+#Prints Wave
+def print_wave(show_wave, show_large_wave):
+    if int(show_wave) == 1:
 
 
+        print(colored("""
+      .-``'.
+    .`   .`
+_.-'     '._ 
+        """, "light_blue"))
+    
+    if int(show_large_wave) == 1:
+        print(colored(""" 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⠾⠿⠿⠯⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣾⠛⠁⠀⠀⠀⠀⠀⠀⠈⢻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠿⠁⠀⠀⠀⢀⣤⣾⣟⣛⣛⣶⣬⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠟⠃⠀⠀⠀⠀⠀⣾⣿⠟⠉⠉⠉⠉⠛⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡟⠋⠀⠀⠀⠀⠀⠀⠀⣿⡏⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡍⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠷⣤⣤⣠⣤⣤⡤⡶⣶⢿⠟⠹⠿⠄⣿⣿⠏⠀⣀⣤⡦⠀⠀⠀⠀⣀⡄
+⢀⣄⣠⣶⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⠚⠋⠉⠀⠀⠀⠀⠀⠀⠈⠛⡛⡻⠿⠿⠙⠓⢒⣺⡿⠋⠁
+⠉⠉⠉⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀
+""", "light_blue"))
+
+#Takes a list as input and rounds each of the elements to the decimal
+def round_decimal(round_list, decimal):
+    rounded_list = list()
+    for num in round_list:
+        rounded_list.append(round(num, decimal))
+    return rounded_list
+
+#Number of forecast days. Max is 7
+def forecast(lat, long, decimal, days=0):
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://marine-api.open-meteo.com/v1/marine"
+    params = {
+        "latitude": lat,
+        "longitude": long,
+        "daily": ["wave_height_max", "wave_direction_dominant", "wave_period_max"],
+        "length_unit": "imperial",
+        "timezone": "auto",
+        "forecast_days": days
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_wave_height_max = round_decimal(daily.Variables(0).ValuesAsNumpy(), decimal)
+    daily_wave_direction_dominant = round_decimal(daily.Variables(1).ValuesAsNumpy(), decimal)
+    daily_wave_period_max = round_decimal(daily.Variables(2).ValuesAsNumpy(), decimal)
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+    time_data = daily_data['date']
+
+    return [daily_wave_height_max, daily_wave_direction_dominant, daily_wave_period_max, time_data]
 
 
     
--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,48 @@
 import helper
 import sys
 
+args = helper.seperate_args(sys.argv)
 #sys cli inputs
 #Defaults. 1 == Show, anything else == hide
-coordinates = helper.get_coordinates(sys.argv)
+coordinates = helper.get_coordinates(args)
 lat = coordinates[0]
 long = coordinates[1]
 city = coordinates[2]
 
-show_art = 1
+show_wave = 1
+show_large_wave = 0
 show_uv = 1
 show_height = 1
 show_direction = 1
 show_period = 1
 show_city = 1
-decimal = helper.extract_decimal(sys.argv)
+show_date = 1
+unit = "imperial"
+decimal = helper.extract_decimal(args)
+forecast_days = helper.get_forecast_days(args)
 
-if "hide_art" in sys.argv:
-    show_art = 0
-if "hide_uv" in sys.argv:
+if "hide_wave" in args:
+    show_wave = 0
+if "show_large_wave" in args:
+    show_large_wave = 1
+if "hide_uv" in args:
     show_uv = 0
-if "hide_height" in sys.argv:
+if "hide_height" in args:
     show_height = 0
-if "hide_direction" in sys.argv:
+if "hide_direction" in args:
     show_direction = 0
-if "hide_period" in sys.argv:
+if "hide_period" in args:
     show_period = 0
-if "hide_location" in sys.argv:
+if "hide_location" in args:
     show_city = 0
-
+if "hide_date" in args:
+    show_date = 0
+if "metric" in args:
+    unit = "metric"
 
 # Calls APIs though python files 
-ocean_data = helper.ocean_information(lat, long, decimal)
-uv_index = helper.get_uv(lat, long, decimal)
+ocean_data = helper.ocean_information(lat, long, decimal, unit)
+uv_index = helper.get_uv(lat, long, decimal, unit)
 
 def main():
     print("\n")
@@ -42,8 +52,10 @@ def main():
         print("No ocean data at this location.")
     else:
         helper.print_location(city, show_city)
-        helper.print_blue_wave(show_art)
+        helper.print_wave(show_wave, show_large_wave)
         helper.print_output(uv_index, ocean_data, show_uv, show_height, show_direction, show_period)
     print("\n")
+    forecast = helper.forecast(lat, long, decimal, forecast_days)
+    helper.print_forecast(forecast, uv_index, ocean_data, show_uv, show_height, show_direction, show_period, show_date)
 
 main()
