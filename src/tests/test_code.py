6b6f1344e9d29d0b3d766bfff829b9de77b92634
ryansurf <ryanfrederich@gmail.com>
2024-05-25 14:19:59 -0700

    github actions pytest

--- src/tests/test_code.py
+++ src/tests/test_code.py
@@ -22,33 +22,36 @@ from api import get_coordinates, get_uv, ocean_information
 
 
 def test_invalid_input():
-        with patch("sys.stdout", new=io.StringIO()) as fake_stdout:
-            extract_decimal(["decimal=NotADecimal"])
-            printed_output = fake_stdout.getvalue().strip()
-            assert(
-                printed_output == "Invalid value for decimal. Please provide an integer."
-            )
+    with patch("sys.stdout", new=io.StringIO()) as fake_stdout:
+        extract_decimal(["decimal=NotADecimal"])
+        printed_output = fake_stdout.getvalue().strip()
+        assert printed_output == "Invalid value for decimal. Please provide an integer."
+
 
 def test_default_input():
-        decimal = extract_decimal([])
-        assert 1 == decimal
+    decimal = extract_decimal([])
+    assert 1 == decimal
+
 
 def test_get_coordinates():
     coordinates = get_coordinates(["loc=santa_cruz"])
     lat = coordinates[0]
     long = coordinates[1]
-    assert(isinstance(lat, (int, float)))
-    assert(isinstance(long, (int, float)))
+    assert isinstance(lat, (int, float))
+    assert isinstance(long, (int, float))
+
 
 def test_get_uv():
     uv = get_uv(37, 122, 2)
-    assert(isinstance(uv, (int, float)))
+    assert isinstance(uv, (int, float))
+
 
 def test_ocean_information():
     ocean = ocean_information(37, 122, 2)
-    assert(isinstance(ocean[0], (int, float)))
-    assert(isinstance(ocean[1], (int, float)))
-    assert(isinstance(ocean[2], (int, float)))
+    assert isinstance(ocean[0], (int, float))
+    assert isinstance(ocean[1], (int, float))
+    assert isinstance(ocean[2], (int, float))
+
 
 def test_main():
     """
@@ -57,4 +60,4 @@ def test_main():
     """
     data_dict = main()
     time.sleep(5)
-    assert(len(data_dict) >= 5)
+    assert len(data_dict) >= 5
