2747e7616480378eb80b5292610417727146becd
ryansurf <ryanfrederich@gmail.com>
2024-05-26 22:12:28 -0700

    localhost in script.js

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,5 +1,5 @@
 // ipAddress = the ip of the machine that is hosting the server
-const ipAddress = '192.168.16.6';
+const ipAddress = 'localhost';
 const port = 8000;
 
 document.getElementById("reportForm").addEventListener("submit", function(event) {
