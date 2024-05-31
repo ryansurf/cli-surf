e37fef8edd2fe3562947154c6169a6f425b160b6
ryansurf <ryanfrederich@gmail.com>
2024-05-31 13:56:44 -0700

    gpt docstrings

--- src/gpt.py
+++ src/gpt.py
@@ -11,6 +11,7 @@ def simple_gpt(surf_summary, gpt_prompt):
     Surf summary is a report of todays data, ex: The surf is 4
     feet with a 10 second period... GPT Prompt is what kind of
     report the user wants, loaded in from the environment vars
+    Using: https://github.com/xtekky/gpt4free
     """
     client = Client()
     response = client.chat.completions.create(
@@ -22,7 +23,11 @@ def simple_gpt(surf_summary, gpt_prompt):
 
 def openai_gpt(surf_summary, gpt_prompt, api_key, model):
     """
+    Surf Summary is a brief summary of the surf data(height, period)
+    and gpt_prompt is the personal report the user wants(reccomend a
+    board, etc). gpt_prompt in .env
     Uses openai's GPT, needs an API key
+    https://platform.openai.com/docs/api-reference/introduction
     """
     client = OpenAI(
         # This is the default and can be omitted
