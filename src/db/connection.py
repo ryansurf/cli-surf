ea44c4d3749e48472d77db1039e1f1252e4b16f0
ryan <ryanfrederich@gmail.com>
2025-11-15 21:20:19 -0800

    readme updates, db dir

---
+++ src/db/connection.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from src.settings import DatabaseSettings
+from src.db import connection
+
+
+
+class Database:
+    # Manages the MongoDB connection 
+    def __init__(self):
+        settings = DatabaseSettings()
+        self.db_uri = settings.DB_URI
+        self.client = None
+        self.db = None
+    
+    def connect(self, db_name='surf'):
+        if not self.client:
+            try:
+                self.client = MongoClient(self.db_uri)
+                self.db = self.client[db_name]
+                print("Database connected successfully")
+            except Exception as e:
+                print("Could not connect to MongoDB: ", e)
+                raise
+        return self.db
+
+    def disconnect(self):
+        # Close the connection 
+        if self.client:
+            self.client.close()
+            self.client = None 
+            self.db = None
+            print("Database connection closed")
+
+db_manager = Database()
+    
---
+++ src/db/operations.py
@@ -0,0 +1,17 @@
+from src.db.connection import db_manager
+
+
+class SurfReportDatabaseOps:
+    # Handles operations to the db 
+    def __init__(self):
+        self.db = db_manager.connect()
+        self.collection = self.db['surfReports']
+    
+    def insert_report(self, report_document):
+        try:
+            rec = self.collection.insert_one(report_document)
+            print("Document inserted with ID: ", rec.inserted_id)
+            return rec.inserted_id
+        except Exception as e:
+            print("Error inserting to the db: ", e)
+            return None
