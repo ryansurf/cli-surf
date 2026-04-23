3118b7bd993de93edae4ca4fcb12f2fc87a07b78
ryan <ryanfrederich@gmail.com>
2026-04-22 21:27:13 -0700

    http caching

--- src/open_meteo.py
+++ src/open_meteo.py
@@ -1,11 +1,14 @@
 import openmeteo_requests
-import requests
+import requests_cache
 from retry_requests import retry
 
 
 def _create_client() -> openmeteo_requests.Client:
-    """Creates a retry-enabled Open-Meteo API client."""
-    retry_session = retry(requests.Session(), retries=5, backoff_factor=0.2)
+    """Creates a cached, retry-enabled Open-Meteo API client."""
+    cache_session = requests_cache.CachedSession(
+        "/tmp/.cache", expire_after=3600
+    )
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     return openmeteo_requests.Client(session=retry_session)
 
 
